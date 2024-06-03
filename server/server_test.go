0025e679af96867c40625cdeb3dd5df2aae87d74
Jesper Bergeskans <jesper.bergeskans@gmail.com>
2024-06-03 21:29:31 +0200

    Adding tests for server functions

---
+++ server/server_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	pb "github.com/julmust/grpc_file_transfer/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	ADDRESS = "localhost:50051"
+)
+
+var c pb.TransferServiceClient
+
+func TestGetBatchTransfer(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := c.GetBatchTransfer(ctx, &pb.TransferInfoMsg{
+		Filename: "test.png",
+	})
+	if err != nil {
+		log.Fatalf("could not create transfer: %v", err)
+	}
+
+	// Check that recieved data is the same as saved data
+	fs, _ := os.ReadFile("test_files/test.png")
+	if !bytes.Equal(fs, res.GetFile()) {
+		t.Fatal("Mismatch!")
+	}
+}
+
+func TestPutBatchTransfer(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	sd, _ := os.ReadFile("test_files/cnc.png")
+	res, err := c.PutBatchTransfer(ctx, &pb.PutBatchTransferMsg{Filename: "test2.png", File: sd})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check that recieved data is the same as saved data
+	newFile, _ := os.ReadFile(res.GetFilename())
+	if !bytes.Equal(sd, newFile) {
+		t.Fatal("Mismatch!")
+	}
+}
+
+func TestMain(m *testing.M) {
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("failed connection: %v", err)
+	}
+	defer lis.Close()
+
+	s := grpc.NewServer()
+	pb.RegisterTransferServiceServer(s, &TransferServer{})
+
+	// Launching server in goroutine as to not block execution
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to server: %v", err)
+		}
+	}()
+
+	conn, err := grpc.NewClient(ADDRESS, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c = pb.NewTransferServiceClient(conn)
+
+	ev := m.Run()
+	os.Exit(ev)
+}
